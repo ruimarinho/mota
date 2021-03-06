c26d98f8b83736fa3ac5a24d84c03e7cb031e005
Rui Marinho <ruipmarinho@gmail.com>
2021-03-06 23:06:14 +0000

    Add support for specifying hosts or IPs

--- browser.go
+++ browser.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
 	"path/filepath"
 	"runtime"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -30,31 +32,76 @@ type Browser struct {
 // DiscoverDevices performs discovery of local devices using the zeroconf (or
 // bonjour) protocol. The lookup is executed against a domain and Shellies
 // are discovered via their web browser service announcement.
-func (b *Browser) DiscoverDevices() ([]Device, error) {
-	log.Infof("Discovering devices on the network for %v seconds...", b.waitTime)
-
+func (b *Browser) DiscoverDevices(hosts []string) ([]Device, error) {
 	devices := make([]Device, 0)
-	resolver, err := zeroconf.NewResolver(nil)
-	if err != nil {
-		return devices, err
-	}
-
-	devicesChan := make(chan Device)
 	entriesChan := make(chan *zeroconf.ServiceEntry)
-	go b.filterShellies(entriesChan, devicesChan)
-
+	devicesChan := make(chan Device)
+	fetchedDevicesChan := make(chan Device)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(b.waitTime))
 	defer cancel()
 
-	err = resolver.Browse(ctx, b.service, b.domain, entriesChan)
-	if err != nil {
-		return devices, err
-	}
-
-	fetchedDevicesChan := make(chan Device)
+	// Filter devices found to shellies only.
+	go b.filterShellies(entriesChan, devicesChan)
 
 	// Fetch settings as soon as devices are found.
-	go b.fetchSettings(ctx, devicesChan, fetchedDevicesChan)
+	go b.fetchSettings(devicesChan, fetchedDevicesChan)
+
+	if len(hosts) == 0 {
+		log.Infof("Discovering devices on the network for %v seconds...", b.waitTime)
+
+		resolver, err := zeroconf.NewResolver(nil)
+		if err != nil {
+			return devices, err
+		}
+
+		err = resolver.Browse(ctx, b.service, b.domain, entriesChan)
+		if err != nil {
+			return devices, err
+		}
+	} else {
+		log.Infof("Preparing to update devices with hosts %v", hosts)
+
+		for _, host := range hosts {
+			if !strings.Contains(host, ":") {
+				host = fmt.Sprintf("%s:80", host)
+			}
+
+			hostString, portString, err := net.SplitHostPort(host)
+			if err != nil {
+				log.Errorf("Host %v is invalid (%v), skipping", host, err)
+				continue
+			}
+
+			port, err := strconv.Atoi(portString)
+			if err != nil {
+				log.Errorf("Port for host %v is invalid (%v), skipping", host, err)
+				continue
+			}
+
+			var resolvedIPs []net.IP
+			parsedIP := net.ParseIP(hostString)
+			if parsedIP != nil {
+				resolvedIPs = append(resolvedIPs, parsedIP)
+			} else {
+				log.Debugf("Host %v does not look like an IP, attempting to resolve as host...", host)
+
+				resolvedIPs, err = net.LookupIP(host)
+				if err != nil {
+					log.Errorf("Host %v is invalid (%v), skipping...", host, err)
+					continue
+				}
+			}
+
+			entriesChan <- &zeroconf.ServiceEntry{
+				HostName: host,
+				Port:     port,
+				AddrIPv4: resolvedIPs,
+				Text:     []string{fmt.Sprintf("id=shelly-%s", host)},
+			}
+		}
+
+		close(entriesChan)
+	}
 
 	for device := range fetchedDevicesChan {
 		devices = append(devices, device)
@@ -68,69 +115,58 @@ func (b *Browser) DiscoverDevices() ([]Device, error) {
 // fetchSettings retrieves the model name and current firmware version
 // via the Settings API from each Shelly discovered. If authentication
 // is required, .netrc authentication is used, if available.
-func (b *Browser) fetchSettings(ctx context.Context, foundDevicesChan chan Device, fetchedDevicesChan chan Device) {
+func (b *Browser) fetchSettings(foundDevicesChan chan Device, fetchedDevicesChan chan Device) {
 	var done sync.WaitGroup
-	searching := true
-
 	var netrcFile *netrc.Netrc
 	netrcPath, err := netrcPath()
 	if err == nil {
 		netrcFile, err = netrc.Parse(netrcPath)
 	}
+	for device := range foundDevicesChan {
+		done.Add(1)
+		go func(device Device, fetchedDevicesChan chan Device) {
+			log.Infof("Fetching settings from %v", device.String())
+			defer done.Done()
 
-	for searching {
-		select {
-		case device := <-foundDevicesChan:
-			done.Add(1)
-			go func(device Device, fetchedDevicesChan chan Device) {
-				log.Debugf("Fetching settings from %v", device.String())
-				defer done.Done()
+			if netrcFile != nil && netrcFile.Machine(device.IP.String()) != nil {
+				log.Debugf("Found netrc entry for device %v", device.String())
 
-				if netrcFile != nil && netrcFile.Machine(device.IP.String()) != nil {
-					log.Debugf("Found netrc entry for device %v", device.String())
+				device.Username = netrcFile.Machine(device.IP.String()).Get("login")
+				device.Password = url.QueryEscape(netrcFile.Machine(device.IP.String()).Get("password"))
+			}
 
-					device.Username = netrcFile.Machine(device.IP.String()).Get("login")
-					device.Password = url.QueryEscape(netrcFile.Machine(device.IP.String()).Get("password"))
-				}
+			client := http.Client{
+				Timeout: 5 * time.Second,
+			}
 
-				client := http.Client{
-					Timeout: 5 * time.Second,
-				}
+			response, err := client.Get(device.GetBaseURL() + "/settings")
+			if err != nil {
+				log.Debug(err)
+				return
+			}
 
-				response, err := client.Get(device.GetBaseURL() + "/settings")
-				if err != nil {
-					log.Debug(err)
-					return
-				}
+			defer response.Body.Close()
 
-				defer response.Body.Close()
+			if response.StatusCode != 200 {
+				log.Errorf("Unable to fetch settings from %v due to incorrect or missing username/password", device.String())
+				return
+			}
 
-				if response.StatusCode != 200 {
-					log.Errorf("Unable to fetch settings from %v due to incorrect or missing username/password", device.String())
-					return
-				}
+			var settings Settings
+			err = json.NewDecoder(response.Body).Decode(&settings)
+			if err != nil {
+				fmt.Println("Error parsing JSON: ", err)
+				return
+			}
 
-				var settings Settings
-				err = json.NewDecoder(response.Body).Decode(&settings)
-				if err != nil {
-					fmt.Println("Error parsing JSON: ", err)
-					return
-				}
+			// Update the device's model type (e.g. SHSW-25) and current firmware.
+			device.Model = settings.Device.Type
+			device.CurrentFWVersion = settings.FW
 
-				// Update the device's model type (e.g. SHSW-25) and current firmware.
-				device.Model = settings.Device.Type
-				device.CurrentFWVersion = settings.FW
+			log.Debugf("Parsed settings from device %v", device.String())
 
-				log.Debugf("Parsed settings from device %v", device.String())
-
-				fetchedDevicesChan <- device
-			}(device, fetchedDevicesChan)
-
-		case <-ctx.Done():
-			// Stop waiting for more devices if the discovery time has passed.
-			close(foundDevicesChan)
-			searching = false
-		}
+			fetchedDevicesChan <- device
+		}(device, fetchedDevicesChan)
 	}
 
 	done.Wait()
@@ -140,8 +176,8 @@ func (b *Browser) fetchSettings(ctx context.Context, foundDevicesChan chan Devic
 // filterShellies rejects any non-Shelly devices from the discovered
 // devices. Shellies announce their identifier (which always starts
 // with shelly*) on the service metadata.
-func (b *Browser) filterShellies(results <-chan *zeroconf.ServiceEntry, devicesChan chan Device) {
-	for entry := range results {
+func (b *Browser) filterShellies(entriesChan <-chan *zeroconf.ServiceEntry, devicesChan chan Device) {
+	for entry := range entriesChan {
 		for _, str := range entry.Text {
 			if strings.HasPrefix(str, "id=shelly") {
 				IP := entry.AddrIPv4[0]
@@ -154,7 +190,9 @@ func (b *Browser) filterShellies(results <-chan *zeroconf.ServiceEntry, devicesC
 		}
 	}
 
-	log.Debug("No more discovered devices left to process")
+	log.Debug("No more discovered devices left to filter")
+
+	close(devicesChan)
 }
 
 // netrcPath attempts to find the .netrc file path depending
--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -10,7 +11,6 @@ import (
 	"testing"
 
 	zeroconf "github.com/grandcat/zeroconf"
-	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -19,58 +19,20 @@ func init() {
 }
 
 func TestNonUpgradable(t *testing.T) {
-	var firmwares string
-	shellyAPIServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+	shellyCloudAPIServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		if req.URL.Path == "/files/firmware" {
-			w.Write([]byte(firmwares))
+			w.Write([]byte(mockSingleDeviceStableVersion("SHSW-25", "http://"+req.Host)))
 			return
 		}
-
 		assert.Fail(t, req.URL.Path)
 	}))
 
-	firmwares = fmt.Sprintf(`{
-		"isok": true,
-		"data": {
-			"SHSW-1": {
-				"url": "%v/firmware/SHSW-1_build.zip",
-				"version": "20200320-123430/v1.6.2@514044b4"
-			},
-			"SHSW-25": {
-				"url": "%v/firmware/SHSW-25_build.zip",
-				"version": "20200309-104051/v1.6.0@43056d58"
-			}
-		}
-	}`, shellyAPIServer.URL, shellyAPIServer.URL)
-
 	deviceServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		assert.Equal(t, "/settings", req.URL.Path)
-		w.Write([]byte(`{
-			"device": {
-				"type": "SHSW-25",
-				"mac": "0D3595FDAE25",
-				"hostname": "shellyswitch25-0D3595FDAE25",
-				"num_outputs": 2,
-				"num_meters": 2,
-				"num_rollers": 1
-			},
-			"name": "",
-			"fw": "20200309-104051/v1.6.0@43056d58",
-			"factory_reset_from_switch": true,
-			"discoverable": false,
-			"build_info": {
-				"build_id": "20200309-104051/v1.6.0@43056d58",
-				"build_timestamp": "2020-03-09T10:40:51Z",
-				"build_version": "1.0"
-			},
-			"hwinfo": {
-				"hw_revision": "prod-191217",
-				"batch_id": 1
-			}
-		}`))
+		w.Write([]byte(mockDeviceSettingsJSON("SHSW-25", "1CAAB5059F90", "20200309-104051/v1.6.0@43056d58")))
 	}))
 
-	updaterServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+	otaServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		assert.Equal(t, "/SHSW-25", req.URL.Path)
 		w.Write([]byte(`{OK}`))
 	}))
@@ -79,22 +41,31 @@ func TestNonUpgradable(t *testing.T) {
 	assert.Nil(t, err)
 	deviceServerPort, err := strconv.Atoi(deviceServerURL.Port())
 	assert.Nil(t, err)
-
-	updaterServerURL, err := url.Parse(updaterServer.URL)
-	updaterServerPort, err := strconv.Atoi(updaterServerURL.Port())
+	otaServerURL, err := url.Parse(otaServer.URL)
+	assert.Nil(t, err)
+	otaServerPort, err := strconv.Atoi(otaServerURL.Port())
 	assert.Nil(t, err)
 
-	zeroconfServer, err := zeroconf.RegisterProxy("shelly-non-upgradable", "_httptest._tcp.", "local.", deviceServerPort, "127.0.0.1", []string{"127.0.0.1"}, []string{"id=shellyswitch25-0D3595", "fw_id=20200309-104051/v1.6.0@43056d58", "arch=esp8266"}, nil)
+	zeroconfServer, err := zeroconf.RegisterProxy("shelly-non-upgradable", "_httptest._tcp.", "local.", deviceServerPort, "shellyswitch25-0D3595FDAE25", []string{"127.0.0.1"}, []string{"id=shellyswitch25-0D3595FDAE25", "fw_id=20200309-104051/v1.6.0@43056d58", "arch=esp8266"}, nil)
 	assert.Nil(t, err)
 	defer zeroconfServer.Shutdown()
 
-	updater, err := NewOTAUpdater(updaterServerPort, "_httptest._tcp.", "local.", 2, WithAPIClient(NewAPIClient(WithBaseURL(fmt.Sprintf(shellyAPIServer.URL)))))
+	otaUpdater, err := NewOTAUpdater(
+		WithAPIClient(
+			NewAPIClient(WithBaseURL(fmt.Sprintf(shellyCloudAPIServer.URL))),
+		),
+		WithServerPort(otaServerPort),
+		WithService("_httptest._tcp."),
+		WithWaitTimeInSeconds(2),
+	)
 	assert.Nil(t, err)
 
-	err = updater.Start()
-	assert.Nil(t, err)
+	err = otaUpdater.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	devices, err := updater.Devices()
+	devices, err := otaUpdater.Devices()
 	assert.Nil(t, err)
 	assert.Len(t, devices, 1)
 
@@ -107,10 +78,9 @@ func TestNonUpgradable(t *testing.T) {
 }
 
 func TestUpgradable(t *testing.T) {
-	var firmwares string
-	shellyAPIServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+	shellyCloudAPIServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		if req.URL.Path == "/files/firmware" {
-			w.Write([]byte(fmt.Sprintf(firmwares)))
+			w.Write([]byte(mockSingleDeviceStableVersion("SHSW-25", "http://"+req.Host)))
 			return
 		}
 
@@ -118,52 +88,15 @@ func TestUpgradable(t *testing.T) {
 			w.Write([]byte(`{OK}`))
 			return
 		}
-
 		assert.Fail(t, req.URL.Path)
 	}))
 
-	firmwares = fmt.Sprintf(`{
-		"isok": true,
-		"data": {
-			"SHSW-1": {
-				"url": "%v/firmware/SHSW-1_build.zip",
-				"version": "20200320-123430/v1.6.2@514044b4"
-			},
-			"SHSW-25": {
-				"url": "%v/firmware/SHSW-25_build.zip",
-				"version": "20200309-104051/v1.6.0@43056d58"
-			}
-		}
-	}`, shellyAPIServer.URL, shellyAPIServer.URL)
-
 	deviceServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		assert.Equal(t, "/settings", req.URL.Path)
-		w.Write([]byte(`{
-			"device": {
-				"type": "SHSW-25",
-				"mac": "1CAAB5059F90",
-				"hostname": "shellyswitch25-1CAAB5059F90",
-				"num_outputs": 2,
-				"num_meters": 2,
-				"num_rollers": 1
-			},
-			"name": "",
-			"fw": "20191127-095418/v1.5.6@0d769d69",
-			"factory_reset_from_switch": true,
-			"discoverable": false,
-			"build_info": {
-				"build_id": "20191127-095418/v1.5.6@0d769d69",
-				"build_timestamp": "2020-03-09T10:40:51Z",
-				"build_version": "1.0"
-			},
-			"hwinfo": {
-				"hw_revision": "prod-191217",
-				"batch_id": 1
-			}
-		}`))
+		w.Write([]byte(mockDeviceSettingsJSON("SHSW-25", "1CAAB5059F90", "20191127-095418/v1.5.6@0d769d69")))
 	}))
 
-	updaterServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+	otaServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		assert.Equal(t, "/SHSW-25", req.URL.Path)
 	}))
 
@@ -171,22 +104,31 @@ func TestUpgradable(t *testing.T) {
 	assert.Nil(t, err)
 	deviceServerPort, err := strconv.Atoi(deviceServerURL.Port())
 	assert.Nil(t, err)
-
-	updaterServerURL, err := url.Parse(updaterServer.URL)
-	updaterServerPort, err := strconv.Atoi(updaterServerURL.Port())
+	otaServerURL, err := url.Parse(otaServer.URL)
+	assert.Nil(t, err)
+	otaServerPort, err := strconv.Atoi(otaServerURL.Port())
 	assert.Nil(t, err)
 
-	zeroconfServer, err := zeroconf.RegisterProxy("shelly-upgradable", "_httptest._tcp.", "local.", deviceServerPort, "127.0.0.1", []string{"127.0.0.1"}, []string{"id=shellyswitch25-1CAAB5", "fw_id=20191127-095418/v1.5.6@0d769d69", "arch=esp8266"}, nil)
+	zeroconfServer, err := zeroconf.RegisterProxy("shelly-upgradable", "_httptest._tcp.", "local.", deviceServerPort, "shellyswitch25-1CAAB5", []string{"127.0.0.1"}, []string{"id=shellyswitch25-1CAAB5", "fw_id=20191127-095418/v1.5.6@0d769d69", "arch=esp8266"}, nil)
 	assert.Nil(t, err)
 	defer zeroconfServer.Shutdown()
 
-	updater, err := NewOTAUpdater(updaterServerPort, "_httptest._tcp.", "local.", 2, WithAPIClient(NewAPIClient(WithBaseURL(fmt.Sprintf(shellyAPIServer.URL)))))
+	otaUpdater, err := NewOTAUpdater(
+		WithAPIClient(
+			NewAPIClient(WithBaseURL(fmt.Sprintf(shellyCloudAPIServer.URL))),
+		),
+		WithServerPort(otaServerPort),
+		WithService("_httptest._tcp."),
+		WithWaitTimeInSeconds(2),
+	)
 	assert.Nil(t, err)
 
-	err = updater.Start()
-	assert.Nil(t, err)
+	err = otaUpdater.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	devices, err := updater.Devices()
+	devices, err := otaUpdater.Devices()
 	assert.Nil(t, err)
 	assert.Len(t, devices, 1)
 
@@ -196,4 +138,298 @@ func TestUpgradable(t *testing.T) {
 		assert.Equal(t, "20191127-095418/v1.5.6@0d769d69", device.CurrentFWVersion)
 		assert.Equal(t, "20200309-104051/v1.6.0@43056d58", device.NewFWVersion)
 	}
+}
+
+func TestUpgradableBeta(t *testing.T) {
+	shellyCloudAPIServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if req.URL.Path == "/files/firmware" {
+			w.Write([]byte(fmt.Sprintf(`{
+				"isok": true,
+				"data": {
+					"SHSW-25": {
+						"url": "%v/firmware/SHSW-25_build.zip",
+						"version": "20200309-104051/v1.6.0@43056d58",
+						"beta_url": "%v/firmware/SHSW-25_build_beta.zip",
+						"beta_ver": "20210122-154345/v1.10.0-rc1@00eeaa9b"
+					}
+				}
+			}`, "http://"+req.Host, "http://"+req.Host)))
+			return
+		}
+
+		if req.URL.Path == "/firmware/SHSW-25_build_beta.zip" {
+			w.Write([]byte(`OK`))
+			return
+		}
+		assert.Fail(t, req.URL.Path)
+	}))
+
+	deviceServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		assert.Equal(t, "/settings", req.URL.Path)
+		w.Write([]byte(mockDeviceSettingsJSON("SHSW-25", "1CAAB5059F90", "20191127-095418/v1.5.6@0d769d69")))
+	}))
+
+	otaServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		assert.Equal(t, "/SHSW-25", req.URL.Path)
+		w.Write([]byte(`{OK}`))
+	}))
+
+	deviceServerURL, err := url.Parse(deviceServer.URL)
+	assert.Nil(t, err)
+	deviceServerPort, err := strconv.Atoi(deviceServerURL.Port())
+	assert.Nil(t, err)
+	otaServerURL, err := url.Parse(otaServer.URL)
+	assert.Nil(t, err)
+	otaServerPort, err := strconv.Atoi(otaServerURL.Port())
+	assert.Nil(t, err)
+
+	zeroconfServer, err := zeroconf.RegisterProxy("shelly-upgradable", "_httptest._tcp.", "local.", deviceServerPort, "shellyswitch25-1CAAB5059F90", []string{"127.0.0.1"}, []string{"id=shellyswitch25-1CAAB5059F90", "fw_id=20191127-095418/v1.5.6@0d769d69", "arch=esp8266"}, nil)
+	assert.Nil(t, err)
+	defer zeroconfServer.Shutdown()
+
+	otaUpdater, err := NewOTAUpdater(
+		WithAPIClient(
+			NewAPIClient(WithBaseURL(fmt.Sprintf(shellyCloudAPIServer.URL))),
+		),
+		WithBetaVersions(true),
+		WithServerPort(otaServerPort),
+		WithService("_httptest._tcp."),
+		WithWaitTimeInSeconds(2),
+	)
+	assert.Nil(t, err)
+
+	err = otaUpdater.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	devices, err := otaUpdater.Devices()
+	assert.Nil(t, err)
+	assert.Len(t, devices, 1)
+
+	for _, device := range devices {
+		assert.Equal(t, device.Port, deviceServerPort)
+		assert.Equal(t, device.IP.String(), deviceServerURL.Hostname())
+		assert.Equal(t, "20191127-095418/v1.5.6@0d769d69", device.CurrentFWVersion)
+		assert.Equal(t, "20210122-154345/v1.10.0-rc1@00eeaa9b", device.NewFWVersion)
+	}
+}
+
+func TestHosts(t *testing.T) {
+	shellyCloudAPIServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if req.URL.Path == "/files/firmware" {
+			w.Write([]byte(mockSingleDeviceStableVersion("SHSW-25", "http://"+req.Host)))
+			return
+		}
+
+		if req.URL.Path == "/firmware/SHSW-25_build.zip" {
+			w.Write([]byte(`{OK}`))
+			return
+		}
+		assert.Fail(t, req.URL.Path)
+	}))
+
+	deviceServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		assert.Equal(t, "/settings", req.URL.Path)
+		w.Write([]byte(mockDeviceSettingsJSON("SHSW-25", "1CAAB5059F90", "20191127-095418/v1.5.6@0d769d69")))
+	}))
+
+	otaServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		assert.Equal(t, "/SHSW-25", req.URL.Path)
+		w.Write([]byte(`{OK}`))
+	}))
+
+	deviceServerURL, err := url.Parse(deviceServer.URL)
+	assert.Nil(t, err)
+	deviceServerPort, err := strconv.Atoi(deviceServerURL.Port())
+	assert.Nil(t, err)
+	otaServerURL, err := url.Parse(otaServer.URL)
+	assert.Nil(t, err)
+	otaServerPort, err := strconv.Atoi(otaServerURL.Port())
+	assert.Nil(t, err)
+
+	zeroconfServer, err := zeroconf.RegisterProxy("shelly-upgradable", "_httptest._tcp.", "local.", deviceServerPort, "shellyswitch25-1CAAB5059F90", []string{"127.0.0.1"}, []string{"id=shellyswitch25-1CAAB5059F90", "fw_id=20191127-095418/v1.5.6@0d769d69", "arch=esp8266"}, nil)
+	assert.Nil(t, err)
+	defer zeroconfServer.Shutdown()
+
+	otaUpdater, err := NewOTAUpdater(
+		WithAPIClient(
+			NewAPIClient(WithBaseURL(fmt.Sprintf(shellyCloudAPIServer.URL))),
+		),
+		WithServerPort(otaServerPort),
+		WithService("_httptest._tcp."),
+		WithWaitTimeInSeconds(2),
+		WithHosts([]string{fmt.Sprintf("127.0.0.1:%v", deviceServerPort)}),
+	)
+	assert.Nil(t, err)
+
+	err = otaUpdater.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	devices, err := otaUpdater.Devices()
+	assert.Nil(t, err)
+	assert.Len(t, devices, 1)
+
+	for _, device := range devices {
+		assert.Equal(t, device.Port, deviceServerPort)
+		assert.Equal(t, device.IP.String(), deviceServerURL.Hostname())
+		assert.Equal(t, "20191127-095418/v1.5.6@0d769d69", device.CurrentFWVersion)
+		assert.Equal(t, "20200309-104051/v1.6.0@43056d58", device.NewFWVersion)
+	}
+}
+
+func TestMalformedHosts(t *testing.T) {
+	shellyCloudAPIServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if req.URL.Path == "/files/firmware" {
+			w.Write([]byte(mockSingleDeviceStableVersion("SHSW-25", "http://"+req.Host)))
+			return
+		}
+
+		if req.URL.Path == "/firmware/SHSW-25_build.zip" {
+			w.Write([]byte(`{OK}`))
+			return
+		}
+		assert.Fail(t, req.URL.Path)
+	}))
+
+	deviceServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		assert.Equal(t, "/settings", req.URL.Path)
+		w.Write([]byte(mockDeviceSettingsJSON("SHSW-25", "1CAAB5059F90", "20191127-095418/v1.5.6@0d769d69")))
+	}))
+
+	otaServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		assert.Equal(t, "/SHSW-25", req.URL.Path)
+		w.Write([]byte(`{OK}`))
+	}))
+
+	deviceServerURL, err := url.Parse(deviceServer.URL)
+	assert.Nil(t, err)
+	deviceServerPort, err := strconv.Atoi(deviceServerURL.Port())
+	assert.Nil(t, err)
+	otaServerURL, err := url.Parse(otaServer.URL)
+	assert.Nil(t, err)
+	otaServerPort, err := strconv.Atoi(otaServerURL.Port())
+	assert.Nil(t, err)
+
+	zeroconfServer, err := zeroconf.RegisterProxy("shelly-upgradable", "_httptest._tcp.", "local.", deviceServerPort, "shellyswitch25-1CAAB5059F90", []string{"127.0.0.1"}, []string{"id=shellyswitch25-1CAAB5059F90", "fw_id=20191127-095418/v1.5.6@0d769d69", "arch=esp8266"}, nil)
+	assert.Nil(t, err)
+	defer zeroconfServer.Shutdown()
+
+	otaUpdater, err := NewOTAUpdater(
+		WithAPIClient(
+			NewAPIClient(WithBaseURL(fmt.Sprintf(shellyCloudAPIServer.URL))),
+		),
+		WithServerPort(otaServerPort),
+		WithService("_httptest._tcp."),
+		WithWaitTimeInSeconds(2),
+		WithHosts([]string{"*"}),
+	)
+	assert.Nil(t, err)
+
+	err = otaUpdater.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	devices, err := otaUpdater.Devices()
+	assert.Nil(t, err)
+	assert.Len(t, devices, 0)
+}
+
+func TestMalformedHostPort(t *testing.T) {
+	shellyCloudAPIServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if req.URL.Path == "/files/firmware" {
+			w.Write([]byte(mockSingleDeviceStableVersion("SHSW-25", "http://"+req.Host)))
+			return
+		}
+
+		if req.URL.Path == "/firmware/SHSW-25_build.zip" {
+			w.Write([]byte(`{OK}`))
+			return
+		}
+		assert.Fail(t, req.URL.Path)
+	}))
+
+	deviceServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		assert.Equal(t, "/settings", req.URL.Path)
+		w.Write([]byte(mockDeviceSettingsJSON("SHSW-25", "1CAAB5059F90", "20191127-095418/v1.5.6@0d769d69")))
+	}))
+
+	otaServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		assert.Equal(t, "/SHSW-25", req.URL.Path)
+		w.Write([]byte(`{OK}`))
+	}))
+
+	deviceServerURL, err := url.Parse(deviceServer.URL)
+	assert.Nil(t, err)
+	deviceServerPort, err := strconv.Atoi(deviceServerURL.Port())
+	assert.Nil(t, err)
+	otaServerURL, err := url.Parse(otaServer.URL)
+	assert.Nil(t, err)
+	otaServerPort, err := strconv.Atoi(otaServerURL.Port())
+	assert.Nil(t, err)
+
+	zeroconfServer, err := zeroconf.RegisterProxy("shelly-upgradable", "_httptest._tcp.", "local.", deviceServerPort, "shellyswitch25-1CAAB5059F90", []string{"127.0.0.1"}, []string{"id=shellyswitch25-1CAAB5059F90", "fw_id=20191127-095418/v1.5.6@0d769d69", "arch=esp8266"}, nil)
+	assert.Nil(t, err)
+	defer zeroconfServer.Shutdown()
+
+	otaUpdater, err := NewOTAUpdater(
+		WithAPIClient(
+			NewAPIClient(WithBaseURL(fmt.Sprintf(shellyCloudAPIServer.URL))),
+		),
+		WithServerPort(otaServerPort),
+		WithService("_httptest._tcp."),
+		WithWaitTimeInSeconds(2),
+		WithHosts([]string{"192.168.1.100::80"}),
+	)
+	assert.Nil(t, err)
+
+	err = otaUpdater.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	devices, err := otaUpdater.Devices()
+	assert.Nil(t, err)
+	assert.Len(t, devices, 0)
+}
+
+func mockDeviceSettingsJSON(model string, mac string, version string) string {
+	return fmt.Sprintf(`{
+		"device": {
+			"type": "%v",
+			"mac": "%v",
+			"hostname": "shelly-%v",
+			"num_outputs": 2,
+			"num_meters": 2,
+			"num_rollers": 1
+		},
+		"name": "",
+		"fw": "%v",
+		"factory_reset_from_switch": true,
+		"discoverable": false,
+		"build_info": {
+			"build_id": "%v",
+			"build_timestamp": "2020-03-09T10:40:51Z",
+			"build_version": "1.0"
+		},
+		"hwinfo": {
+			"hw_revision": "prod-191217",
+			"batch_id": 1
+		}
+	}`, model, mac, mac, version, version)
+}
+
+func mockSingleDeviceStableVersion(model string, serverURL string) string {
+	return fmt.Sprintf(`{
+		"isok": true,
+		"data": {
+			"%v": {
+				"url": "%v/firmware/%v_build.zip",
+				"version": "20200309-104051/v1.6.0@43056d58"
+			}
+		}
+	}`, model, serverURL, model)
 }
